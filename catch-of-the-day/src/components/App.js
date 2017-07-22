41c8fce2bdb71abb5096bc47c6e0361de3e7ed46
Max Magee <maxmagee@users.noreply.github.com>
2017-07-21 19:59:10 -0500

    Created a form to save data to the state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+        // getInitialState
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
+    addFish(fish) {
+        // update our state
+        const fishes = {...this.state.fishes};  // Take a copy of our existing state
+        
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        // set state
+        this.setState({ fishes: fishes });  // Tell React that this piece of the state has updated
+                                            // this.setState({ fishes });  is a short-hand way of writing the above line        
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +33,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
