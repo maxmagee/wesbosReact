323733e845da23cf16bfcf8ce0f421d4df81b406
Max Magee <maxmagee@users.noreply.github.com>
2017-07-30 17:25:47 -0500

    Updating Order State

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,12 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
     render() {
-        const { details } = this.props;
+        const { details, index } = this.props;
+        // These two lines are what the above destructing is doing
+        // const details = this.props.details;  
+        // const index = this.props.index;
+        const isAvailable = details.status === 'available';
+        const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
         return (
             <li className="menu-fish">
                 <img src={details.image} alt={details.name} />
@@ -12,7 +17,7 @@ class Fish extends React.Component {
                     <span className="price">{formatPrice(details.price)}</span>
                 </h3>
                 <p>{details.desc}</p>
-                <button>Add To Order</button>
+                <button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
             </li>
         )        
     }
